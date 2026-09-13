3d675a6a6704e70da5e3513f974efdca072a9f06
Aravind A <aravinda.tech23@gmail.com>
2026-09-13 08:00:13 +0530

    Change default transaction count from 100 to 50

--- transactiongenerator.py
+++ transactiongenerator.py
@@ -77,7 +77,7 @@ def main():
 
     print("\nGenerating sample transactions...\n")
 
-    count = int(os.getenv("TXN_COUNT") or 100)
+    count = int(os.getenv("TXN_COUNT") or 50)
     for _ in range(count):
         transaction = generator.generate_transaction()
         send_transaction(transaction)
